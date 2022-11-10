1c5577aefed4c05aa21139504a4b997c7df4ebfa
sjnoh03 <rsj2003@naver.com>
2022-11-10 14:24:43 +0900

    회원가입 오류

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -78,8 +78,11 @@ router.post("/register-action", function(req, res, next) {
       if(result.length > 0) {
         res.send({alert: "이미 사용중인 id입니다."});
       }else {
-        conn.query(`INSERT INTO user(id, email, password, alert, create_date, update_date) VALUES('${param.id}', '${param.email}', '${cipher(param.password)}', FALSE, now(), now());`)
-        res.send({state: "SUCCESS", id: param.id});
+        conn.query(`INSERT INTO user(id, email, password, alert, create_date, update_date) VALUES('${param.id}', '${param.email}', '${cipher(param.password)}', FALSE, now(), now());`, (err, result, fields) => {
+          if(err) throw err;
+
+          res.send({state: "SUCCESS", id: param.id});
+        })
       }
     })
 

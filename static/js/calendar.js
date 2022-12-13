25e0167e1bcae456ef8f7a7b7b2eb0c099f5504a
sjnoh03 <rsj2003@naver.com>
2022-12-13 16:46:55 +0900

    오류 일부 수정

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -231,7 +231,7 @@ const colIntoLine = ($line, year, month, date, classList, lineHeight, more = fal
               $schedule.classList.add("schedule-start");
 
               setTimeout(e => {
-                $p.style.width = $p.clientWidth;
+                $p.style.width = `${$p.clientWidth}px`;
                 $p.innerText = thisSchedule.name;
               }, 10);
             }

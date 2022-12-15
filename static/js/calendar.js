a872b2a9114bfb14b1d1b6d3ac95f0181272093b
sjnoh03 <rsj2003@naver.com>
2022-12-15 21:50:42 +0900

    test log

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -151,7 +151,7 @@ const colIntoLine = ($line, year, month, date, classList, lineHeight, more = fal
           $line.style.height = `${thisHeight + (thisHeight / (rows - 1) * (moreHeight - rows))}px`;
         }
 
-        console.log(month, date, rows, insert, scheduleTrim);
+        console.log(month, date, rows, insert, scheduleTrim, schedule);
 
         if(rows - 1 <= insert - scheduleTrim && more == false) {
           const $schedule = $calendarDate.querySelector(".schedule:last-of-type");
